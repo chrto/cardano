81bc2ab8ba7d8d94366b56eedb462e2cacfb5f48
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2025-07-02 16:33:31 +0200

    Registered Kupo http service

--- offchain/server/src/service/serviceFactory/serviceFactory.spec.ts
+++ offchain/server/src/service/serviceFactory/serviceFactory.spec.ts
@@ -1,4 +1,4 @@
-import { Fcn } from 'common/types';
+import { Factory, Fcn } from 'common/types';
 import { ISSOConfig } from 'web/server/configuration/loader/sso/ssoConfig.types';
 
 import pluginSdkServiceUnbound from './serviceFactory.unbound';
@@ -9,19 +9,22 @@ import { CardanoService } from 'service/cardano/lucid/cardanoService.types';
 import { CardanoStorage } from 'storage/cardano/lucid/lucid.types';
 import { AppConfig } from 'web/server/configuration/loader/appConfig.types';
 import { PluginSdkSequelize } from 'model/sequelize/modelFactory/modelFactory.types';
+import { ICardanoNodeConfig } from 'web/server/configuration/loader/cardanoNode/cardanoNodeConfig.types';
+import { CardanoKupoService } from 'service/http/kupo/kupoService.types';
 
 describe(`service`, () => {
   describe(`pluginSdkService module`, () => {
-    let authenticationServiceFactory: Fcn<[ISSOConfig], AuthenticationService> = () => ({} as AuthenticationService);
+    let authenticationServiceFactory: Factory<ISSOConfig, AuthenticationService> = () => ({} as AuthenticationService);
     let userServiceFactory: Fcn<[], UserService> = () => ({} as UserService);
-    let cardanoServiceFactory: Fcn<[CardanoStorage], CardanoService> = (_: CardanoStorage) => ({} as CardanoService);
+    let cardanoServiceFactory: Factory<CardanoStorage, CardanoService> = (_: CardanoStorage) => ({} as CardanoService);
+    let cardanoKupoServiceFactory: Factory<ICardanoNodeConfig, CardanoKupoService> = (_: ICardanoNodeConfig) => ({} as CardanoKupoService);
     let pluginSdkService: PluginSdkService;
 
     beforeAll(() => {
       pluginSdkService = pluginSdkServiceUnbound
-        .apply(null, [authenticationServiceFactory, userServiceFactory, cardanoServiceFactory])
-        .apply(null, [{} as AppConfig])
-        .apply(null, [{} as PluginSdkSequelize, {} as CardanoStorage]);
+        (authenticationServiceFactory, userServiceFactory, cardanoServiceFactory, cardanoKupoServiceFactory)
+        ({} as AppConfig)
+        ({} as PluginSdkSequelize, {} as CardanoStorage);
     });
 
     beforeEach(() => {
@@ -30,12 +33,14 @@ describe(`service`, () => {
 
     it(`Should return 'PluginSdkService'`, () => {
       expect(pluginSdkService).toBeObject;
+      expect(Object.keys(pluginSdkService).length).toEqual(6);
 
       expect(pluginSdkService).toHaveProperty('sdkStartStop');
       expect(pluginSdkService).toHaveProperty('sdkTransaction');
       expect(pluginSdkService).toHaveProperty('authenticationService');
       expect(pluginSdkService).toHaveProperty('userService');
       expect(pluginSdkService).toHaveProperty('cardanoService');
+      expect(pluginSdkService).toHaveProperty('cardanoKupoService');
     });
   });
 });
