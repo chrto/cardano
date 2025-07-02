332d4ec1aa033a2578e275e64999c047753b1ad7
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2025-07-02 16:39:47 +0200

    Set axios req query params

--- offchain/server/src/storage/http/axios/requestConfig/requestConfig.types.ts
+++ offchain/server/src/storage/http/axios/requestConfig/requestConfig.types.ts
@@ -2,12 +2,12 @@ import { AxiosRequestConfig, Method } from 'axios';
 import { ParsedUrlQueryInput } from 'querystring';
 import { HEADER_CONTENT_TYPE } from '../axios.types';
 
-type AxiosRequestConfigFactory = (axiosRequestConfig: AxiosRequestConfig) => AxiosRequestConfig;
+export type AxiosRequestConfigFactory = (axiosRequestConfig: AxiosRequestConfig) => AxiosRequestConfig;
 
 export interface RequestConfig {
   setBody: <RB extends ParsedUrlQueryInput> (body: RB, contentType?: HEADER_CONTENT_TYPE) => AxiosRequestConfigFactory;
-  setHeader: <RH> (headers: RH) => AxiosRequestConfigFactory;
+  setHeader: <RH extends object>(headers: RH) => AxiosRequestConfigFactory;
   setMethod: (method: Method) => AxiosRequestConfigFactory;
-  setParams: <RP>(urlParams: RP) => AxiosRequestConfigFactory;
+  setParams: <RP extends object>(urlParams: RP) => AxiosRequestConfigFactory;
   setUrl: (url: string) => AxiosRequestConfigFactory;
 }
