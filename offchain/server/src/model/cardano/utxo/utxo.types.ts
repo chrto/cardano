1b7939b420716a3f791e2e15d322760bfff9e768
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2025-07-02 16:29:48 +0200

    Updated UTxO model

--- offchain/server/src/model/cardano/utxo/utxo.types.ts
+++ offchain/server/src/model/cardano/utxo/utxo.types.ts
@@ -1,4 +1,4 @@
-import { Address, Assets, Datum, DatumHash, TxHash, TxIndex } from '../cardano.types';
+import { Address, Assets, Datum, DatumHash, ScriptHash, TxHash, TxIndex } from '../cardano.types';
 import { Script } from '../script/script.types';
 
 export interface OutputReference {
@@ -11,6 +11,7 @@ export interface TransactionOutput {
   datumHash?: DatumHash;
   datum?: Datum;
   scriptRef?: Script;
+  scriptHash?: ScriptHash;
 }
 
 export interface UTxO extends OutputReference, TransactionOutput {
