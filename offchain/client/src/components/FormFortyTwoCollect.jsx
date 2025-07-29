96cb4dca02fdd8702af94d22d857d89517f67b3d
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2025-07-29 14:46:43 +0200

    wip - adding vesting page

--- offchain/client/src/components/FormFortyTwoCollect.jsx
+++ offchain/client/src/components/FormFortyTwoCollect.jsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import './Form.css';
 import utxoToLucid from '../utils/utxoToLucid';
 import lucidStorage from '../utils/lucid/storage';
+import Modal from './Modal';
+import dispatchData from '../utils/dispatchData';
 
 function FormFortyTwoCollect({ validatorScript, getSelectedScriptUtxos, deselectScriptUtxos }) {
   const [formData, setFormData] = useState({ redeemerValue: 0 });
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [txHash, setTxHash] = useState(null);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -16,18 +20,23 @@ function FormFortyTwoCollect({ validatorScript, getSelectedScriptUtxos, deselect
     const redeemer = Number(formData.redeemerValue)
     const utxos = getSelectedScriptUtxos().map(utxoToLucid)
     if (utxos.length === 0) {
-      return alert("No UTxO has been selected!");
+      setErrorMessage("No UTxO has been selected!");
+      return null;
     }
 
+    const options = { redeemer, redeemerType: 'integer' }
+
     lucidStorage.then(storage =>
-      storage.buildSpendFromContractTx(validatorScript, utxos, redeemer, 'integer')
+      storage.buildSpendFromContractTx(validatorScript, utxos, options)
         .then(storage.signTx)
         .then(storage.submitTx)
-        .then(storage.successHandler)
+        .then(dispatchData(setTxHash))
         .then(() => {
           resetForms()
         })
-        .catch(storage.errorHandler)
+        .catch(e => {
+          setErrorMessage(e.message)
+        })
     )
   };
 
@@ -41,6 +50,13 @@ function FormFortyTwoCollect({ validatorScript, getSelectedScriptUtxos, deselect
     resetForms()
   }
 
+  const handleCloseModal = (e) => {
+    e.preventDefault();
+
+    setErrorMessage(null)
+    setTxHash(null)
+  }
+
   return (
     <div className="form">
       <div className="inputs">
@@ -51,6 +67,21 @@ function FormFortyTwoCollect({ validatorScript, getSelectedScriptUtxos, deselect
         <button type="button" onClick={handleSubmit}>Submit</button>
         <button type="button" onClick={handleReset}>Reset</button>
       </div>
+
+      <Modal isOpen={!!txHash || !!errorMessage} isError={!!errorMessage} onClose={handleCloseModal}>
+        {
+          !!txHash
+            ? <div>
+              <h2>Transaction has been submited.</h2>
+              <p>Transaction Hash: {txHash}</p>
+              <p><a className="form-link" href={`https://preview.cardanoscan.io/transaction/${txHash}`}>Open transaction in explorer</a></p>
+            </div>
+            : <div>
+                <h2>Transaction has been failed.</h2>
+                {!!errorMessage && errorMessage.split("\\n").map(line => <p>{line}</p> )}
+              </div>
+        }
+      </Modal>
     </div>
   );
 }
