aa9985e04774fadc096e8d9ddac84f68826a7e16
Jan Chrtiansky <jan.chrtiansky@gmail.com>
2025-08-02 19:06:18 +0200

    init DB

--- offchain/server/storage/sql/seeds.sql
+++ offchain/server/storage/sql/seeds.sql
@@ -4,3 +4,9 @@ INSERT INTO users
 VALUES
   ('897bd86c-feda-4c17-ab09-20959550899b', 'Admin', 'Adminovic', 'admin.adminovic@company.com', true, 'Admin', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
   ('064615be-15ad-4e10-b06a-6cdc46fa8788', 'Joe', 'Doe', 'joe.doe@company.com', true, 'User', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO scripts
+  (id, type, script, category, title, description, createdAt, updatedAt)
+VALUES
+  ('63a080e2-dc50-4771-a145-93ef6ec24cbe', 'PlutusV2', '49480100002221200101', 'Gift', 'Gift', 'Everybody can spend this..', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+  ('e59382ba-3733-4257-9ce5-43358dd73b73', 'PlutusV2', '581f581d01000022232632498cd5ce24810b6974206275726e7321212100120011', 'Burn', 'Burn', 'Nobody can spend this..', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- offchain/server/storage/sql/tables.sql
+++ offchain/server/storage/sql/tables.sql
@@ -10,3 +10,15 @@ CREATE TABLE IF NOT EXISTS users (
     `updatedAt` DATETIME NOT NULL,
     constraint uq_users_email UNIQUE (email)
 );
+
+CREATE TABLE IF NOT EXISTS scripts (
+    `id` UUID PRIMARY KEY NOT NULL,
+    `type` VARCHAR(10) NOT NULL DEFAULT 'PlutusV2' check (type in ('PlutusV1', 'PlutusV2', 'PlutusV3', 'Native')),
+    `script` VARCHAR NOT NULL,
+    `category` VARCHAR(15) NOT NULL check (category in ('Burn', 'Gift', 'FortyTwo', 'Vesting', 'Unknown')),
+    `title` TINYINT(25) NOT NULL,
+    `description` TINYINT(100),
+    `createdAt` DATETIME NOT NULL,
+    `updatedAt` DATETIME NOT NULL,
+    constraint uq_scripts_script UNIQUE (script)
+);
